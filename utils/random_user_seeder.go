d73e4161ff2115afc0121d4b036479fb46c31699
noctispine <pinefromw@gmail.com>
2022-02-20 23:55:29 +0300

    add some utility functions for tests

---
+++ utils/random_user_seeder.go
@@ -0,0 +1,36 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min, max int) int {
+	return min + rand.Intn(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomMail() string {
+	return RandomString(RandomInt(5, 10)) + "@" + RandomString(RandomInt(2, 5)) + "." + RandomString(3)
+}
+
+func RandomPassword() string {
+	return RandomString(RandomInt(6, 20))
+}
