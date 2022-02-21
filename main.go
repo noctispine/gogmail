ff4bc69d4d24bae65669d1571bc5038575fc6c6e
noctispine <pinefromw@gmail.com>
2022-02-21 16:43:57 +0300

    add db connection

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/noctispine/go-email-app/cmd"
+import (
+	"github.com/noctispine/go-email-app/cmd"
+	"github.com/noctispine/go-email-app/db"
+)
 
 func main() {
+	db.NewDB("local_mails")
 	cmd.Execute()
 }
