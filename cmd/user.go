54bffb199e87474b584171cd51e95edfa0d506f9
noctispine <pinefromw@gmail.com>
2022-02-21 16:47:36 +0300

    add user command to handle with personal data

---
+++ cmd/user.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// confCmd represents the conf command
+var userCmd = &cobra.Command{
+	Use:   "user",
+	Short: "Settings",
+	Long:  `Add, remove, update or list your local savings`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("conf called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(userCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// confCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// confCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
