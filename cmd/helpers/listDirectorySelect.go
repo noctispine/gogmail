e51718672109ab37b47a4f468dd1407f90bde036
noctispine <pinefromw@gmail.com>
2022-02-28 23:47:49 +0300

    feat: hide user info when selected

--- cmd/helpers/listDirectorySelect.go
+++ cmd/helpers/listDirectorySelect.go
@@ -2,44 +2,37 @@ package helper
 
 import (
 	"errors"
-	"fmt"
-	"os"
-	"path/filepath"
+	"io/ioutil"
 )
 
 func MakeSliceFromDir(dirPath string) ([]string, error) {
-	files, err := filePathWalkDir(dirPath)
+	var fileNames []string
+	files, err := ioutil.ReadDir(dirPath)
+
+	if err != nil {
+		return nil, err
+	}
 	if err != nil {
 		return nil, err
 	}
 
-	files = append(files, "../")
-	err = stringSliceReplace(files, len(files)-1, 1)
+	for _, f := range files {
+		fileNames = append(fileNames, f.Name())
+	}
+
+	fileNames = append(fileNames, "../")
+	err = stringSliceReplace(fileNames, len(fileNames)-1, 1)
 	if err != nil {
 		return nil, err
 	}
 
-	files = append(files, "Quit")
-	err = stringSliceReplace(files, len(files)-1, 0)
+	fileNames = append(fileNames, "Quit")
+	err = stringSliceReplace(fileNames, len(fileNames)-1, 0)
 	if err != nil {
 		return nil, err
 	}
-	for _, file := range files {
-		fmt.Println(file)
-	}
 
-	return files, nil
-}
-
-func filePathWalkDir(root string) ([]string, error) {
-	var files []string
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if !info.IsDir() {
-			files = append(files, path)
-		}
-		return nil
-	})
-	return files, err
+	return fileNames, nil
 }
 
 func stringSliceReplace(slice []string, pos1, pos2 int) error {
@@ -49,6 +42,5 @@ func stringSliceReplace(slice []string, pos1, pos2 int) error {
 	temp := slice[pos1]
 	slice[pos1] = slice[pos2]
 	slice[pos2] = temp
-
 	return nil
 }
--- cmd/helpers/promptHelpers.go
+++ cmd/helpers/promptHelpers.go
@@ -3,6 +3,7 @@ package helper
 import (
 	"errors"
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/manifoldco/promptui"
@@ -41,6 +42,80 @@ type PromptUserDetails struct {
 // 	return email, nil
 // }
 
+// isDirectory determines if a file represented
+// by `path` is a directory or not
+func isDirectory(path string) (bool, error) {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+
+	return fileInfo.IsDir(), err
+}
+
+func getWorkingDir() (string, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	return wd, nil
+}
+
+func PromptSelectDir() (string, error) {
+	var files []string
+	var err error
+	var prompt promptui.Select
+	var isDir bool
+	var path string
+
+	path, err = getWorkingDir()
+	if err != nil {
+		return "", err
+	}
+
+	for {
+		files, err = MakeSliceFromDir(path)
+
+		templates := &promptui.SelectTemplates{
+			Label:    "{{ . }}",
+			Active:   "\U000027A4  ({{ . | green }})",
+			Inactive: "  ({{ . | cyan }}) ",
+		}
+
+		prompt = promptui.Select{
+			Label:        "Files",
+			Items:        files,
+			Size:         10,
+			HideSelected: true,
+			Templates:    templates,
+		}
+		if err != nil {
+			return path, err
+		}
+		var i int
+		i, _, err = prompt.Run()
+		if err != nil {
+			return path, err
+		}
+
+		if i == 0 {
+			return "", nil
+		} else if i == 1 {
+			index := strings.LastIndex(path, "/")
+			path = path[:index]
+		} else {
+			path = path + "/" + files[i]
+			isDir, err = isDirectory(path)
+			if err != nil {
+				return path, err
+			}
+			if !isDir {
+				return path, nil
+			}
+		}
+	}
+}
+
 func SelectUser(emails []db.User, size int, showPw bool, cursorPos_optional ...int) promptui.Select {
 	cursorPos := 0
 
@@ -74,12 +149,13 @@ func SelectUser(emails []db.User, size int, showPw bool, cursorPos_optional ...i
 	}
 
 	prompt := promptui.Select{
-		Label:     "Emails",
-		Items:     emails,
-		Searcher:  searcher,
-		Templates: templates,
-		Size:      size,
-		CursorPos: cursorPos,
+		Label:        "Emails",
+		Items:        emails,
+		Searcher:     searcher,
+		Templates:    templates,
+		Size:         size,
+		CursorPos:    cursorPos,
+		HideSelected: true,
 	}
 
 	return prompt
